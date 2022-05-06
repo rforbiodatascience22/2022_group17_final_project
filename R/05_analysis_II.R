a282366ff1cad6fea254e9f7aeae5177e7c3022a
narong-narnua <narnua@food.dtu.dk>
2022-05-06 09:18:57 +0000

    edited

--- R/05_analysis_II.R
+++ R/05_analysis_II.R
@@ -1,13 +1,12 @@
 # Load libraries ----------------------------------------------------------
-library("tidyverse")
-
+library(broom)
 
 # Define functions --------------------------------------------------------
-source(file = "R/99_project_functions.R")
+source(file = "R/05_analysis.R")
 
 
 # Load data ---------------------------------------------------------------
-my_data_clean_aug <- read_tsv(file = "data/03_my_data_clean_aug.tsv")
+
 
 
 # Wrangle data ------------------------------------------------------------
@@ -19,8 +18,43 @@ my_data_clean_aug %>% ...
 
 
 # Visualise data ----------------------------------------------------------
-my_data_clean_aug %>% ...
 
+library(broom)
+
+matrix_d0_scale <- matrix_d0 %>%
+  as_tibble %>% 
+  select(P01579, P05231, P10145, O95786) %>% 
+  prcomp(center = TRUE, scale. = TRUE) 
+
+matrix_d0_scale %>% tidy("pcs")
+
+matrix_d0_scale %>% tidy("pcs") %>% 
+  ggplot(aes(x = PC, y = percent)) +
+  geom_col() +
+  theme_bw()
+
+matrix_d0_scale %>% tidy("samples")
+
+
+matrix_d0_scale_aug <- matrix_d0_scale %>% augment(metadata)
+
+matrix_d0_scale_aug %>% 
+  ggplot(aes(x = .fittedPC1, y = .fittedPC2, colour = "COVID")) +
+  geom_point()
+
+######### 
+matrix_d0_scale<- matrix_d0/merge_df %>% 
+  select(P01579, P05231, P10145, O95786) %>% 
+  prcomp(center = TRUE, scale. = TRUE)
+
+autoplot(matrix_d0_scale,
+         data = metadata, colour = factor(metadata$covid),
+         fill = factor(metadata$covid),
+         frame = TRUE,
+         frame.type = "norm"
+) +
+  labs(color = "COVID", fill = "COVID", title = "PCA - Protein Expression of Covid +/- patients at timepoint 0 ")
+########
 
 # Write data --------------------------------------------------------------
 write_tsv(...)
