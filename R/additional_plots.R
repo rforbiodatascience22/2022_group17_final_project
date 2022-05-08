869428b04d01af7d8db351fb1a039eb78b9fdf08
ewelinabid <s205980@student.dtu.dk>
2022-05-08 14:56:38 +0000

    fixed_plots, deleted not useful code

--- R/additional_plots.R
+++ R/additional_plots.R
@@ -1,38 +1,26 @@
+grouped_by_patientid = merge_df %>% group_by(subject_id, Age_cat, HEART, LUNG, Resp_Symp) %>%
+  summarise(Count=sum(subject_id))
 
-#age distribution
-merge_df %>%
-  ggplot(mapping = aes(x = Age_cat))+
-  geom_histogram(binwidth = 0.5)+
-  labs()
-
-#NPX distribution
-npx_distribution <- ggplot(data=merge_df, aes(x=NPX)) +
-  geom_histogram(alpha=0.5) + 
-  labs(title='NPX distribution') +
-  facet_wrap(~BMI_cat)
-npx_distribution
-
-pl1 = ggplot(data = merge_df,
+pl1 = ggplot(data = grouped_by_patientid,
              aes(x = Age_cat,
-                 color = factor(HEART))) +
-  geom_hline(yintercept = 0, linetype = "dashed") +
+                 fill = factor(HEART))) +
   geom_histogram(binwidth= 0.5, alpha = 0.5) +
-  scale_fill_manual(labels = c("No Heart Disease", "Heart Disease")) +
-  labs(x = "Patient Age Group",
-       y = "Count",
-       fill = "Heart disease") +
+  scale_fill_manual(labels = c("No Heart Disease", "Heart Disease"), values=c("#CC0066", "#FF9999")) +
+  labs(title = "Heart disease occurences in patients", x = "Patient Age Group",
+       y = "Number of patients",
+       fill="Heart Disease") +
   theme_classic(base_size = 12) +
   theme(legend.position = "bottom")
 
 pl1
 
-pl2 = ggplot(data = merge_df,
+pl2 = ggplot(data = grouped_by_patientid,
              aes(x = Age_cat,
-                 color = factor(LUNG))) +
-  geom_hline(yintercept = 0, linetype = "dashed") +
+                 fill = factor(LUNG))) +
   geom_histogram(binwidth= 0.5, alpha = 0.5) +
-  scale_fill_manual(labels = c("No Lung Disease", "Lung Disease")) +
-  labs(x = "Patient Age Group",
+  scale_fill_manual(labels = c("No Lung Disease", "Lung Disease"), values=c("#CC0066", "#FF9999")) +
+  labs(title = "Lung disease occurences in patients",
+       x = "Patient Age Group",
        y = "Count",
        fill = "Lung Disease") +
   theme_classic(base_size = 12) +
@@ -41,20 +29,19 @@ pl2
 
 pl3 = ggplot(data = merge_df,
              aes(x = Age_cat,
-                 color = factor(Resp_Symp))) +
-  geom_hline(yintercept = 0, linetype = "dashed") +
+                 fill = factor(Resp_Symp))) +
   geom_histogram(binwidth= 0.5, alpha = 0.5) +
-  scale_fill_manual(labels = c("no respiratory Symptoms", "respiratory symptoms")) +
-  labs(x = "Patient Age Group",
+  scale_fill_manual(labels = c("no respiratory Symptoms", "respiratory symptoms"),values=c("#CC0066", "#FF9999")) +
+  labs(title = "Respiratory symptoms occurences in patients",
+       x = "Patient Age Group",
        y = "Count",
        fill = "Heart disease") +
   theme_classic(base_size = 12) +
   theme(legend.position = "bottom")
 pl3
-library("patchwork")
+
 
 # Compute the frequency
-library(dplyr)
 counts0 <- descriptive_df %>%
   group_by(abs_neut_0_cat) %>%
   summarise(counts = n())
