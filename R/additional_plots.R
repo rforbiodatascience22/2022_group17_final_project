659eba5880d7fc8d5dd0f48ab10fce20cf8ebe58
ewelinabid <s205980@student.dtu.dk>
2022-05-08 11:44:32 +0000

    moreplots

--- R/additional_plots.R
+++ R/additional_plots.R
@@ -1,4 +1,55 @@
-#aqge distribution
+descriptive_df <- merge_df %>%
+  mutate(COVID = case_when(
+    COVID == 0 ~ "COVID -",
+    COVID == 1 ~ "COVID +",
+  ))
+descriptive_df <-  descriptive_df  %>%
+  mutate("Age_cat" = case_when(
+    Age_cat == 1 ~ "20-34",
+    Age_cat == 2 ~ "36-49",
+    Age_cat == 3 ~ "50-64",
+    Age_cat == 4 ~ "65-79",
+    Age_cat == 5 ~ "80+"
+  ))
+descriptive_df <-  descriptive_df  %>%
+  mutate("BMI_cat" = case_when(
+    BMI_cat == 0 ~ "18.5 underweight",
+    BMI_cat == 1 ~ "8.5-24.9 normal",
+    BMI_cat == 2 ~ "25.0-29.9 overweight",
+    BMI_cat == 3 ~ "30.0-39.9 obese",
+    BMI_cat == 4 ~ "40 severely obese",
+    BMI_cat == 5 ~ "unknown"
+  ))
+descriptive_df <-  descriptive_df  %>%
+  mutate("abs_neut_0_cat" = case_when(
+    abs_neut_0_cat == 1 ~ '0-0.99',
+    abs_neut_0_cat == 2 ~ '1.0-3.99',
+    abs_neut_0_cat == 3 ~ '4.0-7.99',
+    abs_neut_0_cat == 4 ~ '8.0-11.99',
+    abs_neut_0_cat == 5 ~ '12+'
+  ))
+
+descriptive_df <-  descriptive_df  %>%
+  mutate("abs_neut_3_cat" = case_when(
+    abs_neut_3_cat  == 1 ~ '0-0.99',
+    abs_neut_3_cat  == 2 ~ '1.0-3.99',
+    abs_neut_3_cat  == 3 ~ '4.0-7.99',
+    abs_neut_3_cat  == 4 ~ '8.0-11.99',
+    abs_neut_3_cat  == 5 ~ '12+'
+  ))
+
+#descriptive_df %>% drop_na(abs_neut_7_cat)
+
+descriptive_df <-  descriptive_df  %>%
+  mutate("abs_neut_7_cat" = case_when(
+    abs_neut_7_cat  == 1 ~ '0-0.99',
+    abs_neut_7_cat  == 2 ~ '1.0-3.99',
+    abs_neut_7_cat  == 3 ~ '4.0-7.99',
+    abs_neut_7_cat  == 4 ~ '8.0-11.99',
+    abs_neut_7_cat  == 5 ~ '12+',
+  ))
+
+#age distribution
 merge_df %>%
   ggplot(mapping = aes(x = Age_cat))+
   geom_histogram(binwidth = 0.5)+
@@ -50,61 +101,6 @@ pl3 = ggplot(data = merge_df,
   theme_classic(base_size = 12) +
   theme(legend.position = "bottom")
 pl3
-
-descriptive_df <- merge_df %>%
-  mutate(COVID = case_when(
-    COVID == 0 ~ "COVID -",
-    COVID == 1 ~ "COVID +",
-  ))
-descriptive_df <-  descriptive_df  %>%
-  mutate("Age_cat" = case_when(
-    Age_cat == 1 ~ "20-34",
-    Age_cat == 2 ~ "36-49",
-    Age_cat == 3 ~ "50-64",
-    Age_cat == 4 ~ "65-79",
-    Age_cat == 5 ~ "80+"
-  ))
-
-descriptive_df <-  descriptive_df  %>%
-  mutate("BMI_cat" = case_when(
-    BMI_cat == 0 ~ "18.5 underweight",
-    BMI_cat == 1 ~ "8.5-24.9 normal",
-    BMI_cat == 2 ~ "25.0-29.9 overweight",
-    BMI_cat == 3 ~ "30.0-39.9 obese",
-    BMI_cat == 4 ~ "40 severely obese",
-    BMI_cat == 5 ~ "unknown"
-  ))
-
-descriptive_df <-  descriptive_df  %>%
-  mutate("abs_neut_0_cat" = case_when(
-    abs_neut_0_cat == 1 ~ '0-0.99',
-    abs_neut_0_cat == 2 ~ '1.0-3.99',
-    abs_neut_0_cat == 3 ~ '4.0-7.99',
-    abs_neut_0_cat == 4 ~ '8.0-11.99',
-    abs_neut_0_cat == 5 ~ '12+'
-  ))
-
-
-descriptive_df <-  descriptive_df  %>%
-  mutate("abs_neut_3_cat" = case_when(
-    abs_neut_3_cat  == 1 ~ '0-0.99',
-    abs_neut_3_cat  == 2 ~ '1.0-3.99',
-    abs_neut_3_cat  == 3 ~ '4.0-7.99',
-    abs_neut_3_cat  == 4 ~ '8.0-11.99',
-    abs_neut_3_cat  == 5 ~ '12+'
-  ))
-
-#descriptive_df %>% drop_na(abs_neut_7_cat)
-
-descriptive_df <-  descriptive_df  %>%
-  mutate("abs_neut_7_cat" = case_when(
-    abs_neut_7_cat  == 1 ~ '0-0.99',
-    abs_neut_7_cat  == 2 ~ '1.0-3.99',
-    abs_neut_7_cat  == 3 ~ '4.0-7.99',
-    abs_neut_7_cat  == 4 ~ '8.0-11.99',
-    abs_neut_7_cat  == 5 ~ '12+',
-  ))
-
 library("patchwork")
 
 # Compute the frequency
---
+++ R/neutralization_analysis.R
@@ -0,0 +1,108 @@
+#
+total <- merge(merge_df, neut_assay_clean, by="subject_id")
+
+total <- total %>%
+  mutate(COVID = case_when(
+    COVID == 0 ~ "Negative",
+    COVID == 1 ~ "Positive",
+  ))
+
+#neutralisation level vs covid status
+neut_d0 <- total %>% filter(Day==0)
+neut_d3 <- total %>% filter(Day==3)
+neut_d7 <- total %>% filter(Day==7)
+
+scat0 <- ggplot(data=neut_d0, aes(x=COVID, y=Percent.Neutralization)) + geom_point(color='darkblue') + geom_boxplot(color='darkred') +
+  coord_fixed()+
+  labs(title = "Neutralisation level in patients", subtitle = "Day 0") +
+  ylab('Neutralisation level [%]') + 
+  xlab('Covid Status')
+scat0
+
+scat3 <- ggplot(data=neut_d3, aes(x=COVID, y=Percent.Neutralization)) + geom_point(color='darkblue') + geom_boxplot(color='darkred') +
+  coord_fixed()+
+  labs(title = "Neutralisation level in patients with Covid", subtitle = "Day 3") +
+  ylab('Neutralisation level [%]') + 
+  xlab('Covid Status')
+scat3
+
+scat7 <- ggplot(data=neut_d7, aes(x=COVID, y=Percent.Neutralization)) + geom_point(color='darkblue') + geom_boxplot(color='darkred') +
+  coord_fixed()+
+  labs(title = "Neutralisation level in patients with Covid", subtitle = "Day 7") +
+  ylab('Neutralisation level [%]') + 
+  xlab('Covid Status')
+scat7
+
+
+Acuity_df <- total %>%
+  mutate(Severity = case_when(
+    Acuity_max == 1 ~ "Severe",
+    Acuity_max == 2 ~ "Severe",
+    Acuity_max == 3 ~ "Non - Severe",
+    Acuity_max == 4 ~ "Non - Severe",
+    Acuity_max == 5 ~ "Non - Severe"
+  ))
+
+#Comparing severe and non severe patients and theirs neutralisation level
+Acuity_df <- Acuity_df %>% mutate(Acuity_max = case_when(
+   Acuity_max == 1 ~ "Death",
+   Acuity_max == 2 ~ "Intubated",
+   Acuity_max == 3 ~ "Hospitalized, supplementary O2",
+   Acuity_max == 4 ~ "Hospitalized,  no supplementary O2",
+   Acuity_max == 5 ~ "No Hospialziation"
+ ))
+
+Acuity_p <- ggplot(data = Acuity_df, mapping=aes(x=COVID, y=Percent.Neutralization, fill=Acuity_max)) + geom_boxplot()+
+  theme(legend.position = "bottom") + labs(title = "Neutralisation level in patients", subtitle = "Comparison of neutralisation in case of different hospitalisation levels") +
+  ylab('Neutralisation level [%]') + 
+  xlab('Covid Status')
+ Acuity_p
+
+severity_df <- filter(Acuity_df, Day %in% c('0', '3', '7'))
+unique(severity_df$Day) 
+neut_severity1 <- ggplot(data = severity_df , mapping=aes(x = Day, y = Percent.Neutralization, fill = Severity)) + 
+  geom_boxplot() +
+  labs(title = "Boxplots of neutralization levels in non-severe and severe patients over time") +
+  ylab('Neutralisation level [%]') +
+  theme_dark()
+
+neut_severity1
+
+# Proportion of patients with neutralization levels over time and by severity level
+
+neutralisation_levels <- severity_df %>% mutate(Neutralisation = case_when(
+  Percent.Neutralization < 0.25 ~ "0-25",
+  Percent.Neutralization >= 0.25 & Percent.Neutralization < 0.50 ~ "25-50",
+  Percent.Neutralization >= 0.50 & Percent.Neutralization < 0.75 ~ "50-75",
+  Percent.Neutralization >= 0.75 & Percent.Neutralization <= 1 ~ "75-100"
+))
+
+severe_df <- filter(neutralisation_levels, Severity == 'Severe') %>%
+  group_by(subject_id, Day, Neutralisation) 
+
+non_severe_df <- filter(neutralisation_levels, Severity =="Non - Severe") %>%
+  group_by(Neutralisation, Day) 
+
+neut_severe <- ggplot(data = severe_df , mapping = aes(x = Day, fill = Neutralisation)) + 
+  geom_bar(position = 'stack') +
+  theme_dark() +
+  ylab('Proportion of patients') +
+  theme(axis.text.y=element_blank(),  #remove y axis labels
+        axis.ticks.y=element_blank(),
+        legend.position = "none"
+  )
+
+
+
+neut_non_severe <- ggplot(data = severe_df , mapping=aes(x = Day, fill = Neutralisation)) + 
+  geom_bar(position = 'stack') +
+  theme_dark() +
+  ylab('') +
+  theme(axis.text.y=element_blank(),  
+        axis.ticks.y=element_blank(),
+  )
+
+neut_severe + neut_non_severe 
+ 
+
+
